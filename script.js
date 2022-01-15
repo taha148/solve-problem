7e96e99ce26c0ae9acc0d141be07289d0d7fb702
taha148 <64436466+taha148@users.noreply.github.com>
2022-01-15 20:16:13 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,42 @@
+/* const example1 = [
+    "hello world",
+    "this is a test",
+    "this is an example"
+];
+
+const getCommonChars=(arrString=[])=> {
+    // Write your code here
+  let JoinWord=""
+  arrString.forEach(word=>JoinWord+= `${word} `)
+  return [...JoinWord.trim()]
+}
+
+console.log(getCommonChars(example1)) */
+
+
+// const example1 = [
+// 	[0, 0, 1, 1, 0, 0],
+// 	[0, 1, 1, 1, 0, 0],
+// 	[0, 1, 0, 0, 0, 1],
+// 	[0, 1, 0, 0, 1, 1],
+// 	[1, 0, 0, 1, 1, 1],
+// ];
+
+// const countForest=(arr) =>{
+//     // Write your code here
+//     return arr.reduce((total,insideArr)=>{
+//     insideArr.forEach((item,index)=>{
+//          if(index>0&&index<insideArr.length){
+
+//              if(insideArr[index-1]==1&&insideArr[index]==1&&insideArr[index+1]==1){
+//           total+=1
+//         }
+//             }
+
+//     })
+//        return total
+//     },0)
+// }
+
+
+// console.log(countForest(example1))
